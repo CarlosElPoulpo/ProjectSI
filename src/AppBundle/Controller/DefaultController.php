d54c21439bed200c200172a78d09a335efb22598
Nathan De Pachtere <de.pachtere.nat@gmail.com>
2016-04-17 12:56:43 +0200

    Gestion des utilisateurs: +Register +Login +LogOut +Profile (show/edit) +Reset password +Email Confirmation Ajout du template de base et des librairies CSS et JS: +fontawesome +bootstrap +materialize +Jquery

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,8 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
     }
+
+    public function menuAction(){
+        return $this->render('elements/menu.html.twig');
+    }
 }
